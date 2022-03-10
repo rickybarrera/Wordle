eef93f04d27b4020f22138559a63d00743f834d3
ricky barrera <rickybarrrera0@gmail.com>
2022-03-10 09:07:21 -0800

    newer changes

--- js/main.js
+++ js/main.js
@@ -9,21 +9,64 @@ let currentGuess = [
     [],
     [],
 ];
+
 // shows in console what clicks/important to eventually playing
-const keys = document.querySelectorAll(".keyboard-row button");
-for (let i = 0; i < keys.length; i++) {
-    keys[i].onclick = ({ target }) => {
-        const key = target.getAttribute("data-key")
-    currentGuess[0].push(key);
-    console.log(currentGuess[0]);
-    };
-}
+const keys = document.querySelector("#keyboard-container");
+// console.log(keys.length);
+keys.addEventListener('click', function(evt){
+    if(currentGuess[0].length < 5){
+        const key = evt.target.getAttribute("data-key")
+        console.log(key);
+        currentGuess[0].push(key);
+        console.log(currentGuess[0]);
+        console.log(currentGuess);
+    }
+    let answerCheck = currentGuess[0].join('');
+    console.log(answerCheck);
+    function checkWinner() {
+        if (answerCheck === rightGuessString)
+        { console.log("you win");
+   
+        }
+    }
+})
+    
+
+// 
+// for (let i = 0; i < keys.length; i++) {
+//     keys[i].onclick = ({ target }) => {
+//         
+//     };
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // need to make function that puts guesses into squares and keeps
 // track of them im thinking into empty arrays and win if string 
 // matches rightGuessString
+let allGuesses = [];
+currentGuess.join(',');
+console.log(allGuesses);
+// for (let currentGuess = 0; currentGuess < 5;    )
 
 
 // like function says you guessed it creates squares
+
+
+// FUNCTIONS    
 function createSquares() {
     const gameBoard = document.getElementById("board");
 
@@ -36,18 +79,34 @@ function createSquares() {
 
 }
 })
+// function handlePlayerClick(evt) {
+//     const 
+//     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // word bank n win game
-const wordBank = ['about','anger', 'music']
+const wordBank = ['ABOUT','ANGER', 'MUSIC']
 const guessNumber = 6;
 let guessesLeft = guessNumber;
 
 let nextLetter = 0;
 let rightGuessString = wordBank[Math.floor(Math.random () * wordBank.length)]
  // winners function
- function checkWinner () {
-     if (currentGuess === rightGuessString)
-     { console.log("you win")
+ 
 
-     }
- }
+
